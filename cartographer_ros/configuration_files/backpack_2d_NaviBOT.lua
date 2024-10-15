260909c45fa8ff6d578bab68b100b6c652083a1b
Xinjue Zou <hit_zou@126.com>
2024-10-15 15:07:01 +0800

    update params for NaviBOT

--- cartographer_ros/configuration_files/backpack_2d_NaviBOT.lua
+++ cartographer_ros/configuration_files/backpack_2d_NaviBOT.lua
@@ -36,8 +36,8 @@ options = {
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
-  rangefinder_sampling_ratio = 0.1,
-  odometry_sampling_ratio = 0.1,
+  rangefinder_sampling_ratio = 1.,
+  odometry_sampling_ratio = 0.5,
   fixed_frame_pose_sampling_ratio = 1.,
   imu_sampling_ratio = 1.,
   landmarks_sampling_ratio = 1.,
@@ -48,7 +48,7 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 TRAJECTORY_BUILDER_2D.num_accumulated_range_data = 1
-TRAJECTORY_BUILDER_2D.use_imu_data = true
+TRAJECTORY_BUILDER_2D.use_imu_data = false
 -- POSE_GRAPH.optimize_every_n_nodes = 0
 
 return options
